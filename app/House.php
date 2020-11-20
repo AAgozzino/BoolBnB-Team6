b01262a5635a4ede09e0addc9dadd9cbf4ddac4b
Davide Cesare <davidereaper@gmail.com>
2020-11-20 17:52:18 +0100

    Created links on models

--- app/House.php
+++ app/House.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class House extends Model
 {
-    //
+    public function images()
+    {
+        return $this->hasMany('App\Image');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany('App\Message');
+    }
+
+    public function type()
+    {
+        return $this->belongsTo('App\Type');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function views()
+    {
+        return $this->hasMany('App\View');
+    }
+
+    public function services()
+    {
+        return $this->belongsToMany('App\Service');
+    }
+
+    public function sponsors()
+    {
+        return $this->belongsToMany('App\Sponsor');
+    }
 }
