5d112c786e73b4d8502fc0bdeaca4a8bf4def3e7
Anna Agozzino <anna.agozzino91@gmail.com>
2020-11-26 18:37:39 +0100

    WIP Header graphic

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,3 +1,42 @@
 <header>
-    <h2>Header</h2>
+    <div class="container">
+        <div class="row">
+            {{-- Logo --}}
+            <div class="header_logo col-2">
+                <img src="#" alt="BoolBnB logo">
+            </div>
+            {{-- /Logo --}}
+
+            {{-- Header menu --}}
+            <div class="header_menu col">
+                <div class="nav">
+                    <ul class="nav_list">
+                        <li class="nav_list_item active">
+                            <a href="#">Appartamenti</a>
+                        </li>
+                        <li class="nav_list_item">
+                            <a href="#">Sponsorizzazioni</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            {{-- Header menu --}}
+
+            {{-- Authentication --}}
+            <div class="header_login col">
+                @if (Route::has('login'))
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                @endif
+            </div>
+            {{-- /Authentication --}}
+        </div>
+    </div>
 </header>
