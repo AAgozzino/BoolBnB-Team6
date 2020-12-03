6fa9911e13275270f3838d341772c76cefaa4972
Davide Cesare <davidereaper@gmail.com>
2020-12-03 16:10:11 +0100

    init layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>BoolBnB</title>
-    {{-- Link Style CSS --}}
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-</head>
-<body>
-    {{-- Partial Header --}}
-    @include('layouts.partials.header')
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>BoolBnB</title>
+        {{-- Link Style CSS --}}
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body>
+        {{-- Partial Header --}}
+        @include('layouts.partials.header')
 
-    {{-- Yeld main --}}
-    @yield('main-section')
+        {{-- Yield main --}}
+        @yield('main-section')
 
-    {{-- Partial Footer --}}
-    @include('layouts.partials.admin__footer')
-    <script src='{{asset('js/app.js')}}'></script>
-</body>
+        {{-- Partial Footer --}}
+        @include('layouts.partials.admin__footer')
+        <script src='{{asset('js/app.js')}}'></script>
+    </body>
 </html>
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,3 +1,65 @@
 <header>
-    <h2>Header</h2>
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+
+                </ul>
+
+                <!-- Central Side Of Navbar/ Input search -->
+                <form action="{{route("houses.search")}}" method="POST">
+                    @csrf
+                    @method("POST")
+                    <input type="text" name="address" id="address-input" placeholder="Inserisci indirizzo del tuo alloggio" value="{{old('address')}}">
+                    <p>Hai cercato: <strong id="address-value"></strong></p>
+                    <input id="latitude" type="hidden" name="lat" value="">
+                    <input id="longitude" type="hidden" name="lon" value="">
+                    {{-- <input type="number" placeholder="20" id="radius"> --}}
+                    <input type="submit" value="Invia">
+                </form>
+
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Registrati') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>
