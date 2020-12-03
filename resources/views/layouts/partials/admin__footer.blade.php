184f1400beee6dcde69d910de2816abb34f962fd
Anna Agozzino <anna.agozzino91@gmail.com>
2020-12-03 18:35:44 +0100

    wip footer

--- resources/views/layouts/partials/admin__footer.blade.php
+++ resources/views/layouts/partials/admin__footer.blade.php
@@ -1,5 +1,58 @@
 <footer class="footer_cl">
     <div class="container">
-        <img src="" alt="">
+        <div class="row">
+            <div id="contributors" class="footer_menu col-12 col-lg-4">
+                <h4>Controbutors></i></h4>
+                <ul class="footer_menu_list">
+                    <li class="footer_menu_list_item">
+                        <a href="https://www.linkedin.com/in/davide-cesare-a53045200/"><i class="fab fa-linkedin"></i> Davide Cesare</a>
+                    </li>
+                    <li class="footer_menu_list_item">
+                        <a href="https://www.linkedin.com/in/annaagozzino/"><i class="fab fa-linkedin"></i> Anna Agozzino</a>
+                    </li>
+                    <li class="footer_menu_list_item">
+                        <a href="https://www.linkedin.com/in/lauracolotto/"><i class="fab fa-linkedin"></i> Laura Colotto</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer_menu col-12 col-lg-4">
+                <h4>Stack</h4>
+                <ul class="footer_menu_list">
+                    <li class="footer_menu_list_item">HTML</li>
+                    <li class="footer_menu_list_item">Scss</li>
+                    <li class="footer_menu_list_item">JavaScript</li>
+                    <li class="footer_menu_list_item">Laravel</li>
+                    <li class="footer_menu_list_item">Algolia Places</li>
+                    <li class="footer_menu_list_item">Chart.js</li>
+                    <li class="footer_menu_list_item">Braintree.js</li>
+                </ul>
+            </div>
+            <div class="footer_menu col-12 col-lg-4">
+                <h4>Thanks to</h4>
+                <ul class="footer_menu_list">
+                    <li class="footer_menu_list_item">
+                        <a href="https://www.linkedin.com/in/samuele-madrigali-a4905a76/"><i class="fab fa-linkedin"></i> Samuele Madrigali</a>
+                    </li>
+                    <li class="footer_menu_list_item">
+                        <a href="https://www.linkedin.com/in/chiara-passaro-90812b10/"><i class="fab fa-linkedin"></i> Chiara Passaro</a>
+                    </li>
+                    <li class="footer_menu_list_item">
+                        <a href="https://www.linkedin.com/in/gennari-sviluppatore-php/"><i class="fab fa-linkedin"></i> Enrico Gennari</a>
+                    </li>
+                    <li class="footer_menu_list_item">
+                        <a href="https://www.linkedin.com/in/elisabetta-dah%C3%B2-a85083121/"><i class="fab fa-linkedin"></i> Elisabetta Dah&ograve;</a>
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+        <div id="copyright" class="row">
+            <div class="col">
+                <div class="inline">&copy; BoolBnB - Team6</div>
+                <div class="inline">Classe&#35;17</div>
+                <div class="inline">Year 2020</div>
+
+            </div>
+        </div>
     </div>
 </footer>
