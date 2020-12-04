2c8f540d4c842ef39957cb9ddccf80c061336caf
Anna Agozzino <anna.agozzino91@gmail.com>
2020-12-04 11:51:03 +0100

    Footer layout to merge

--- resources/views/layouts/partials/admin__footer.blade.php
+++ resources/views/layouts/partials/admin__footer.blade.php
@@ -1,58 +1,109 @@
 <footer class="footer_cl">
-    <div class="container">
-        <div class="row">
-            <div id="contributors" class="footer_menu col-12 col-lg-4">
-                <h4>Controbutors></i></h4>
-                <ul class="footer_menu_list">
-                    <li class="footer_menu_list_item">
-                        <a href="https://www.linkedin.com/in/davide-cesare-a53045200/"><i class="fab fa-linkedin"></i> Davide Cesare</a>
-                    </li>
-                    <li class="footer_menu_list_item">
-                        <a href="https://www.linkedin.com/in/annaagozzino/"><i class="fab fa-linkedin"></i> Anna Agozzino</a>
-                    </li>
-                    <li class="footer_menu_list_item">
-                        <a href="https://www.linkedin.com/in/lauracolotto/"><i class="fab fa-linkedin"></i> Laura Colotto</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="footer_menu col-12 col-lg-4">
-                <h4>Stack</h4>
-                <ul class="footer_menu_list">
-                    <li class="footer_menu_list_item">HTML</li>
-                    <li class="footer_menu_list_item">Scss</li>
-                    <li class="footer_menu_list_item">JavaScript</li>
-                    <li class="footer_menu_list_item">Laravel</li>
-                    <li class="footer_menu_list_item">Algolia Places</li>
-                    <li class="footer_menu_list_item">Chart.js</li>
-                    <li class="footer_menu_list_item">Braintree.js</li>
-                </ul>
-            </div>
-            <div class="footer_menu col-12 col-lg-4">
-                <h4>Thanks to</h4>
-                <ul class="footer_menu_list">
-                    <li class="footer_menu_list_item">
-                        <a href="https://www.linkedin.com/in/samuele-madrigali-a4905a76/"><i class="fab fa-linkedin"></i> Samuele Madrigali</a>
-                    </li>
-                    <li class="footer_menu_list_item">
-                        <a href="https://www.linkedin.com/in/chiara-passaro-90812b10/"><i class="fab fa-linkedin"></i> Chiara Passaro</a>
-                    </li>
-                    <li class="footer_menu_list_item">
-                        <a href="https://www.linkedin.com/in/gennari-sviluppatore-php/"><i class="fab fa-linkedin"></i> Enrico Gennari</a>
-                    </li>
-                    <li class="footer_menu_list_item">
-                        <a href="https://www.linkedin.com/in/elisabetta-dah%C3%B2-a85083121/"><i class="fab fa-linkedin"></i> Elisabetta Dah&ograve;</a>
-                    </li>
-                    
-                </ul>
+    {{-- Section footer menus --}}
+    <section id="footer_menu">
+        <div class="container">
+            <div class="row justify-content-between">
+                {{-- Contributors --}}
+                <div id="contributors" class="col-12 col-md-3">
+                    <h4>Controbutors</h4>
+                    <ul class="footer_menu_list">
+                        <li class="footer_menu_list_item">
+                            <a href="https://www.linkedin.com/in/davide-cesare-a53045200/"><i class="fab fa-linkedin"></i> Davide Cesare</a>
+                        </li>
+                        <li class="footer_menu_list_item">
+                            <a href="https://www.linkedin.com/in/annaagozzino/"><i class="fab fa-linkedin"></i> Anna Agozzino</a>
+                        </li>
+                        <li class="footer_menu_list_item">
+                            <a href="https://www.linkedin.com/in/lauracolotto/"><i class="fab fa-linkedin"></i> Laura Colotto</a>
+                        </li>
+                    </ul>
+                </div>
+                {{-- /Contributors --}}
+
+                {{-- Stack --}}
+                <div class="footer_menu col-12 col-md-3">
+                    <h4>Stack</h4>
+                    <ul class="footer_menu_list">
+                        <li class="footer_menu_list_item">HTML</li>
+                        <li class="footer_menu_list_item">Scss</li>
+                        <li class="footer_menu_list_item">JavaScript</li>
+                        <li class="footer_menu_list_item">Laravel</li>
+                        <li class="footer_menu_list_item">Algolia Places</li>
+                        <li class="footer_menu_list_item">Chart.js</li>
+                        <li class="footer_menu_list_item">Braintree.js</li>
+                    </ul>
+                </div>
+                {{-- /Stack --}}
+
+                {{-- Thanks to --}}
+                <div class="footer_menu col-12 col-md-3">
+                    <h4>Thanks to</h4>
+                    <ul class="footer_menu_list">
+                        <li class="footer_menu_list_item">
+                            <a href="https://www.linkedin.com/in/samuele-madrigali-a4905a76/"><i class="fab fa-linkedin"></i> Samuele Madrigali</a>
+                        </li>
+                        <li class="footer_menu_list_item">
+                            <a href="https://www.linkedin.com/in/chiara-passaro-90812b10/"><i class="fab fa-linkedin"></i> Chiara Passaro</a>
+                        </li>
+                        <li class="footer_menu_list_item">
+                            <a href="https://www.linkedin.com/in/gennari-sviluppatore-php/"><i class="fab fa-linkedin"></i> Enrico Gennari</a>
+                        </li>
+                        <li class="footer_menu_list_item">
+                            <a href="https://www.linkedin.com/in/elisabetta-dah%C3%B2-a85083121/"><i class="fab fa-linkedin"></i> Elisabetta Dah&ograve;</a>
+                        </li>
+                    </ul>
+                </div>
+                {{-- /Thanks to --}}
             </div>
         </div>
-        <div id="copyright" class="row">
-            <div class="col">
-                <div class="inline">&copy; BoolBnB - Team6</div>
-                <div class="inline">Classe&#35;17</div>
-                <div class="inline">Year 2020</div>
+    </section>   
+     {{--/Section footer menus  --}}
+
+     {{-- Section copyright --}}
+     <section id="copyright">
+        <div class="container">
+            <div class="row">
+                {{-- Copyright --}}
+                <div class="col copyright">
+                    <div class="wrapper d_flex">
+                        <div class="elm">&copy; BoolBnB Team6</div>
+                        <div class="elm">Boolean Classe&#35;17</div>
+                        <div class="elm">2020</div>
+                    </div>
+                </div>
+                {{-- /Copyright --}}
+                
+                {{-- Language --}}
+                <div class="col-2 language">
+                    <div class="wrapper d_flex">
+                        <div class="language_menu">
+                            <i class="fas fa-globe"></i> 
+                            <div class="language_text">Italiano (IT)</div>
+                        </div>
+                    </div>
+                </div>
+                {{-- /Language --}}
+
+                {{-- Social --}}
+                <div class="col-2 social">
+                    <div class="wrapper d_flex">
+                        <ul class="social_menu_list">
+                            <li class="social_menu_list_item">
+                                <a href="#"><i class="fab fa-facebook-square"></i></a>
+                            </li>
+                            <li class="social_menu_list_item">
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </li>
+                            <li class="social_menu_list_item">
+                                <a href="#"><i class="fab fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                {{-- /Social --}}
 
             </div>
         </div>
-    </div>
+    </section>
+    {{-- /Section copyright --}}
 </footer>
